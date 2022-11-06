38aa2df6c1c65f95596be7bec6ffb4f602ad4656
Konstantin <kdanilov1911@gmail.com>
2022-11-06 22:16:46 +0300

    is done

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,23 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int a =0;
+        int b=arr.length-1;
+        int tmp;
+
+        while (b > a) {
+            tmp = arr[b];
+            arr[b] = arr[a];
+            arr[a] = tmp;
+            a++;
+            b--;
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+
 
     }
 }
